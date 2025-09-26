b9705552cb7bcdf20fb3225dec8c8698266ca986
Kaike Vieira <szlachata.vieira@gmail.com>
2025-09-26 11:39:39 -0300

    feat: wrap MainRoutes with AuthProvider for authentication context

--- help-desk-frontend/src/main.jsx
+++ help-desk-frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import MainRoutes from './Routes/Routes.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <AuthProvider>
     <MainRoutes/>
+      </AuthProvider>
   </BrowserRouter>
 )
