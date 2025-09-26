048e12eaa0ef500d871a530b12a0d5243337fd14
Kaike Vieira <szlachata.vieira@gmail.com>
2025-09-26 15:47:00 -0300

    feat: add user service methods for findByUsername, create, and update operations

--- help-desk-backend/src/services/userService.js
+++ help-desk-backend/src/services/userService.js
@@ -1,5 +1,11 @@
 import { userRepo } from '../repositories/userRepo.js';
 
 export const userService = {
-  list: (q) => userRepo.list(q)
+  list: (q) => userRepo.list(q),
+
+  findByUsername: (username) => userRepo.findByUsername(username),
+
+  create: (data) => userRepo.create(data),
+
+  update: (id, data) => userRepo.update(id, data),
 };
