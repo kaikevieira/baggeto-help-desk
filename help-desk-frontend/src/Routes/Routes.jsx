2b7833196c2147cc4a61fbfa9d8f5f8f6630977a
Kaike Vieira <szlachata.vieira@gmail.com>
2025-09-26 11:40:47 -0300

    feat: update routing structure to include additional pages and improve navigation

--- help-desk-frontend/src/Routes/Routes.jsx
+++ help-desk-frontend/src/Routes/Routes.jsx
@@ -1,16 +1,34 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import PrivateRoute from "../context/PrivateRoute.jsx";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
+import Settings from "../pages/Settings";
+import Team from "../pages/Team";
+import TicketDetails from "../pages/TicketDetails";
+import TicketNew from "../pages/TicketNew";
+import Tickets from "../pages/Tickets";
 
 const MainRoutes = () => {
+  return (
+    <Routes>
+      {/* pública */}
+      <Route path="/login" element={<Login />} />
 
-    return (
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-    );
- 
+      {/* protegidas */}
+      <Route element={<PrivateRoute />}>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/tickets" element={<Tickets />} />
+        <Route path="/tickets/new" element={<TicketNew />} />
+        <Route path="/tickets/:id" element={<TicketDetails />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/settings" element={<Settings />} />
+      </Route>
+
+      {/* fallback */}
+      <Route path="*" element={<Navigate to="/dashboard" replace />} />
+    </Routes>
+  );
 };
 
 export default MainRoutes;
