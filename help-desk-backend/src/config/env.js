6b1da442c60455568cc1217603c48b9cbdbf27be
Kaike Vieira <szlachata.vieira@gmail.com>
2025-09-26 11:29:14 -0300

    feat: create env.js para gerenciar as variaveis do ambiente

---
+++ help-desk-backend/src/config/env.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+
+export const ENV = {
+  PORT: process.env.PORT || 4000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:5173',
+  COOKIE_SECURE: process.env.COOKIE_SECURE === 'true',
+  ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN || '15m',
+  REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN || '7d',
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET
+};
