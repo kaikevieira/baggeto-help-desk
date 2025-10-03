c7bc83db9d1f356c3c0cac20989c6e67904e2414
Kaike Vieira <szlachata.vieira@gmail.com>
2025-10-03 10:39:42 -0300

    fix: update server.js to listen on all interfaces and simplify startup log message

--- help-desk-backend/src/server.js
+++ help-desk-backend/src/server.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 import { ENV } from './config/env.js';
 
-app.listen(ENV.PORT, () => {
-  console.log(`API rodando em http://localhost:${ENV.PORT} (${ENV.NODE_ENV})`);
-});
+
+app.listen(ENV.PORT, '0.0.0.0', () => console.log('up'));
+
