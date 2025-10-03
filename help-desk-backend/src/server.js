d7e4f695dbdc5f30787c6bc1fef6496907d22184
Kaike Vieira <szlachata.vieira@gmail.com>
2025-10-03 11:51:33 -0300

    fix: update server.js to set host to Koyeb deployment URL for proper listening

--- help-desk-backend/src/server.js
+++ help-desk-backend/src/server.js
@@ -1,7 +1,7 @@
 import app from './app.js';
 import { ENV } from './config/env.js';
 
-const host = '0.0.0.0';
+const host = 'uncertain-dione-kaikevieira-a7451037.koyeb.app';
 app.listen(ENV.PORT, host, () => {
 	console.log(`up on http://${host}:${ENV.PORT}`);
 });
