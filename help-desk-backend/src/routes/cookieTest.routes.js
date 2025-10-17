f44669d296d9af209221cade8d2c55d1d934cab0
Kaike Vieira <szlachata.vieira@gmail.com>
2025-10-17 09:26:14 -0300

    refactor: remove iOS cookie testing components and related utilities

--- help-desk-backend/src/routes/cookieTest.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { cookieTestController } from '../controllers/cookieTestController.js';
-
-const router = Router();
-
-router.post('/test', cookieTestController.test);
-router.get('/verify', cookieTestController.verify);
-
-export default router;
